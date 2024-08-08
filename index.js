99d22219f4be7bdb376f2639e86f80fd7df34995
TanzeelAhmad@Github <Tanzeelofficial@outlook.com>
2024-08-08 10:45:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+const systemGeneratedNo = Math.random() * 10;
+const answers = await inquirer.prompt([
     {
-        message: "Select one of the operators to perform action",
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        type: "number",
+        name: "userGuess",
+        message: "Write your guess b/w 1 to 10: ",
     },
 ]);
-// Condiontional Statement
-if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
-    console.log(answer.firstNumber);
-    console.log(answer.secondNumber);
-    console.log("error");
-}
-else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
+const { userGuess } = answers;
+console.log(userGuess, "userGuess", systemGeneratedNo, "sYs");
+if (userGuess === systemGeneratedNo) {
+    console.log("Yeah your answer is correct \n You Win! ");
 }
 else {
-    console.log("Please select valid operator");
+    console.log("Please try again better luck next time!");
 }
