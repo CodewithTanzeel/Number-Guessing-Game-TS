a9d0fddb4e4794c3d440cd13f4f10e94889e1105
TanzeelAhmad@Github <Tanzeelofficial@outlook.com>
2024-08-08 10:45:07 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,34 +1,27 @@
-import inquirer from "inquirer";//importing the library of inquirer
+import inquirer from "inquirer";
+type ansType = {
+  userGuess: number;
+};
 
-const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+const systemGeneratedNo = Math.random() * 10;//This willl generatea a random number which will be multiplied by
+// as shown!!
+
+const answers: ansType = await inquirer.prompt([
   {
-    message: "Select one of the operators to perform action",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    type: "number",//This restricts that only number is allowed to input!!!
+    name: "userGuess",//Declaring value of key  to userguess beacause user will guess!!
+    message: "Write your guess b/w 1 to 10: ",//This will print on user screen !!!
   },
-]);//await inquirer.promt is user here in order to obtain Data from the user
+]);
 
+const { userGuess } = answers;
 
-
-// Condiontional Statement
-if (answer.operator === "Addition") {
-  console.log(answer.firstNumber + answer.secondNumber);
-  console.log(answer.firstNumber);
-  console.log(answer.secondNumber);
-  
-  console.log("error");//will run else wise
-  
-} else if (answer.operator === "Subtraction") {
-  console.log(answer.firstNumber - answer.secondNumber);
-} else if (answer.operator === "Multiplication") {
-  console.log(answer.firstNumber * answer.secondNumber);
-} else if (answer.operator === "Division") {
-  console.log(answer.firstNumber / answer.secondNumber);
+console.log(userGuess, "userGuess", systemGeneratedNo, "sYs");
+if (userGuess === systemGeneratedNo) {
+  console.log("Yeah your answer is correct \n You Win! ");
 } else {
-  console.log("Please select valid operator")
+  console.log("Please try again better luck next time!");
 }
 
-// The pure logic behind this code is if else statement and arthematic operaters!!!!!
+
+// Again using if else staments we are declaring that what happpens when the system generated No and the user guess message matches as statement above in the console of first if!!!!!!
